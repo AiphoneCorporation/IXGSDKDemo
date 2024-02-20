b86aecc2f07c21a7ab0c2dcaf3f721f623524941
Michael <michael.sweeney@aiphone.com>
2024-02-20 10:40:00 -0800

    various UI updates

--- IXGSDKDemo/ViewControllers/RegistrationConfirmationViewController.swift
+++ IXGSDKDemo/ViewControllers/RegistrationConfirmationViewController.swift
@@ -11,15 +11,13 @@ import AiphoneIntercomCorePkg
 class RegistrationConfirmationViewController: UIViewController, UITextFieldDelegate {
     var registrationManager: RegistrationManager!
     @IBOutlet weak var stationNameTextField: UITextField!
-    var stations = [IXGStation]()
+    
     var name: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         stationNameTextField.delegate = self
-        
-        stationNameTextField.text = "Enter name"//update text input default text to default name
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {//user tapped Done
@@ -28,9 +26,9 @@ class RegistrationConfirmationViewController: UIViewController, UITextFieldDeleg
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let text = textField.text else { return }//if value is null, do nothing
-        
-        name = text// update with new name
+        guard let text = textField.text else { return }
+        guard text != "" else { return }
+        name = text
     }
     
     @IBAction func confirmationButtonTapped(_ sender: Any) {
